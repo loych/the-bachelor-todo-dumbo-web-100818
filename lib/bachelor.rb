a16d5ad45c31ed300dc626a1b7658f3334ee28f6
Lois Choi <insil321@hotmail.com>
2018-10-06 23:22:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,9 @@ def get_first_name_of_season_winner(data, season)
       if detail == "Winner"
         name = contestant["name"]
         return name.split(' ' ).first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
