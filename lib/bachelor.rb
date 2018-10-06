606dcee1309b63b7c6ea821eae434f5713663ffa
Lois Choi <insil321@hotmail.com>
2018-10-06 23:15:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,8 @@
 def get_first_name_of_season_winner(data, season)
   "season 19".each do |topics, details|
     if topics = "status"
-      "status".find do ("winner")
+      "status".find do ("Winner")
+      return ("Winner").first word
 end
 
 def get_contestant_name(data, occupation)
